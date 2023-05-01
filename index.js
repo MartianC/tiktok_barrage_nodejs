12637b0375ff95408be75e320917f87e7a977b2a
成玉仁 <martian-cheng@live.com>
2023-05-01 11:22:17 +0800

    test

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const Barrage = class {
     }
     runServer() {
         let _this = this
+        console.log('join: ' + this.option.join)
         if (this.option.join) {
             this.observer = new MutationObserver((mutationsList) => {
                 for (let mutation of mutationsList) {
